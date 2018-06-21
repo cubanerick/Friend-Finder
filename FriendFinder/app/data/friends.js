e87225e25cf429cfc66c6eece8dc4ccdee8de587
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-06-20 22:14:31 -0700

    dependencies & get/post routes added

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+    {
+        "name":"Erick",
+        "photo":"#",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Kyle",
+        "photo":"#",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+  ];
+  
+  
+  module.exports = friends;
+  
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,4 +1,30 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// ***** ADD routes*****
+
+
+
+
+
+
+
+
+
+
+
+
+// *********
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
+});
