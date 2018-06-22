ddb0bc790387580d0c5845771a1e587a54e61e29
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-06-21 20:02:14 -0700

    pre-heroku

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -8,9 +8,44 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
 
-        friends.push(req.body);
-        res.json(true);
+        friends.push({  
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: [
+                parseInt(req.body.scores[0]),
+                parseInt(req.body.scores[1]),
+                parseInt(req.body.scores[2]),
+                parseInt(req.body.scores[3]),
+                parseInt(req.body.scores[4]),
+                parseInt(req.body.scores[5]),
+                parseInt(req.body.scores[6]),
+                parseInt(req.body.scores[7]),
+                parseInt(req.body.scores[8]),
+                parseInt(req.body.scores[9]),
+            ]
+        });
 
+        var bestmatchscore = 50;
+        var bestmatch = {};
+
+        for(var i = 0; i < friends.length - 1; i++){
+            var last = friends[friends.length - 1].scores;
+            var option = friends[i].scores;
+            var subtotal = 0;
+
+            for(var j = 0; j < 10; j++){
+                subtotal += Math.abs(last[j]-option[j])
+            }
+
+            if(subtotal < bestmatchscore) {
+                bestmatchscore = subtotal;
+                bestmatch = friends[i];
+            }
+        }
+        console.log(bestmatch);
+        res.json(bestmatch);
     });
+
+
     
 }
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -2,28 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-// ***** ADD routes*****
-
-
-
-
-
-
-
-
-
-
-
-
-// *********
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("Listening on PORT: " + PORT);
